4ea8fb72f977b3100f426371ccfd38e52c19b43d
Scott Barron <scott@elitists.net>
2011-01-09 21:09:12 -0500

    Can keep score for arbitrary number of players now

--- coffee/x01.coffee
+++ coffee/x01.coffee
@@ -1,37 +1,52 @@
 class x01
-  constructor:  ->
-    @paper = Raphael('board')
-    @score = Raphael('score')
+  constructor:  (starting_points) ->
+    @paper  = Raphael('board')
+    @hits   = 0
+    @player = 0
+    @turns  = 0
+    @starting_points = starting_points
 
     new DartBoard(this);
 
-  start: (starting_points) ->
+  start: (players) ->
     @game_over = false
-    @current_score = starting_points
-    this.updateScore()
+    @players = []
+    for i in [0...players]
+      @players[i] = {r: Raphael("score#{i}"), score: @starting_points}
+      this.setScoreText(@starting_points, @players[i])
+
+  restart: ->
+    @hits   = 0
+    @player = 0
+    @turns  = 0
+    @game_over = false
+    for player in @players
+      player.score = @starting_points
+      this.setScoreText(@starting_points, player)
 
   hit: (score) ->
     return if @game_over
+    @hits += 1
+    @turns += 1 if @hits % 3 == 0
 
     switch this.validateScore(score)
       when "good"
-        @current_score -= score
-        this.updateScore()
+        @players[@player].score -= score
+        this.setScoreText(@players[@player].score, @players[@player])
       when "win"
-        this.setScoreText('WIN!')
+        this.setScoreText('WIN!', @players[@player])
         @game_over = true
       when "bust"
-        this.setScoreText("BUST! #{@current_score}")
+        this.setScoreText("BUST!", @players[@player])
 
-  updateScore: ->
-    this.setScoreText(@current_score)
+    @player = @turns % @players.length
 
-  setScoreText: (score) ->
-    @score.clear()
-    text = @score.print(4, 24, score, @score.getFont("Chalkduster"), 48)
+  setScoreText: (score, player) ->
+    player.r.clear()
+    text = player.r.print(4, 24, score, player.r.getFont("Chalkduster"), 48)
     text.attr({fill: "white"})
 
   validateScore: (score) ->
-    return "good" if score < @current_score
-    return "win"  if @current_score == score
-    return "bust" if score > @current_score
+    return "good" if score < @players[@player].score
+    return "win"  if @players[@player].score == score
+    return "bust" if score > @players[@player].score
--- html/js/x01.js
+++ html/js/x01.js
@@ -1,49 +1,80 @@
 var x01;
 x01 = (function() {
-  function x01() {
+  function x01(starting_points) {
     this.paper = Raphael('board');
-    this.score = Raphael('score');
+    this.hits = 0;
+    this.player = 0;
+    this.turns = 0;
+    this.starting_points = starting_points;
     new DartBoard(this);
   }
-  x01.prototype.start = function(starting_points) {
+  x01.prototype.start = function(players) {
+    var i, _results;
     this.game_over = false;
-    this.current_score = starting_points;
-    return this.updateScore();
+    this.players = [];
+    _results = [];
+    for (i = 0; (0 <= players ? i < players : i > players); (0 <= players ? i += 1 : i -= 1)) {
+      this.players[i] = {
+        r: Raphael("score" + i),
+        score: this.starting_points
+      };
+      _results.push(this.setScoreText(this.starting_points, this.players[i]));
+    }
+    return _results;
+  };
+  x01.prototype.restart = function() {
+    var player, _i, _len, _ref, _results;
+    this.hits = 0;
+    this.player = 0;
+    this.turns = 0;
+    this.game_over = false;
+    _ref = this.players;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      player = _ref[_i];
+      player.score = this.starting_points;
+      _results.push(this.setScoreText(this.starting_points, player));
+    }
+    return _results;
   };
   x01.prototype.hit = function(score) {
     if (this.game_over) {
       return;
     }
+    this.hits += 1;
+    if (this.hits % 3 === 0) {
+      this.turns += 1;
+    }
     switch (this.validateScore(score)) {
       case "good":
-        this.current_score -= score;
-        return this.updateScore();
+        this.players[this.player].score -= score;
+        this.setScoreText(this.players[this.player].score, this.players[this.player]);
+        break;
       case "win":
-        this.setScoreText('WIN!');
-        return this.game_over = true;
+        this.setScoreText('WIN!', this.players[this.player]);
+        this.game_over = true;
+        break;
       case "bust":
-        return this.setScoreText("BUST! " + this.current_score);
+        this.setScoreText("BUST!", this.players[this.player]);
     }
+    return this.player = this.turns % this.players.length;
   };
-  x01.prototype.updateScore = function() {
-    return this.setScoreText(this.current_score);
-  };
-  x01.prototype.setScoreText = function(score) {
+  x01.prototype.setScoreText = function(score, player) {
     var text;
-    this.score.clear();
-    text = this.score.print(4, 24, score, this.score.getFont("Chalkduster"), 48);
+    player.r.clear();
+    text = player.r.print(4, 24, score, player.r.getFont("Chalkduster"), 48);
     return text.attr({
       fill: "white"
     });
   };
   x01.prototype.validateScore = function(score) {
-    if (score < this.current_score) {
+    if (score < this.players[this.player].score) {
       return "good";
     }
-    if (this.current_score === score) {
+    if (this.players[this.player].score === score) {
       return "win";
     }
-    if (score > this.current_score) {
+    if (score > this.players[this.player].score) {
       return "bust";
     }
   };
